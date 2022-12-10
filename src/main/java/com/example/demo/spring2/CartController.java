f56d59690e3f50c81db9a2811d6b24894d4aa756
mpasichniuk <marypasichnyuk@gmail.com>
2022-12-10 17:11:48 +0100

    homework9 9

--- src/main/java/com/example/demo/spring2/CartController.java
+++ src/main/java/com/example/demo/spring2/CartController.java
@@ -3,36 +3,42 @@ package com.example.demo.spring2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/cart")
 @RequiredArgsConstructor
 public class CartController {
     private final CartService cartService;
+    private final CartConverter cartConverter;
 
-    @GetMapping
-    public Cart getCurrentCart() {
-        return cartService.getCurrentCart();
+    @GetMapping("/generate_id")
+    public StringResponse generateGuestCartId() {
+        return new StringResponse(UUID.randomUUID().toString());
     }
 
-    @GetMapping("/add/{productId}")
-    public void addProductToCart(@PathVariable Long productId) {
-        cartService.addToCart(productId);
+    @GetMapping("/{guestCartId}")
+    public CartDTO getCurrentCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId) {
+        String currentCartId = selectCartId(username, guestCartId);
+        return cartConverter.entityToDto(cartService.getCurrentCart(currentCartId));
     }
-    @GetMapping("/increase/{productId}")
-    public void increaseProductToCart(@PathVariable Long productId){
-        cartService.increaseProductToCart(productId);
-    }
-    @GetMapping("/decrease/{productId}")
-    public void decreaseProductInCart( @PathVariable Long productId){
-        cartService.decreaseProductInCart(productId);
-    }
-    @GetMapping("/delete/{productId}")
-    public void deleteProductInCart(@PathVariable Long productId){
-        cartService.deleteProductInCart(productId);
 
+    @GetMapping("/{guestCartId}/add/{productId}")
+    public void addProductToCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId, @PathVariable Long productId) {
+        String currentCartId = selectCartId(username, guestCartId);
+        cartService.addToCart(currentCartId, productId);
     }
-    @GetMapping("/clear")
-    public void clearCart(){
-        cartService.clear();
+
+    @GetMapping("/{guestCartId}/clear")
+    public void clearCurrentCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId) {
+        String currentCartId = selectCartId(username, guestCartId);
+        cartService.clearCurrentCart(currentCartId);
+    }
+
+    private String selectCartId(String username, String guestCartId) {
+        if (username != null) {
+            return username;
+        }
+        return guestCartId;
     }
 }
